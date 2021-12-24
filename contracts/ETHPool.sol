92c14c196742aa7c8ec156d2dc6d10d044640909
jeissoni_22 <jeissoni_22@hotmail.com>
2021-12-24 12:32:17 -0500

    start challenge

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -3,16 +3,51 @@ pragma solidity ^0.8.0;
 
 contract ETHPool {
 
-    mapping(address => uint)
-
     //state variable
 
+    uint256 private ultimaFechaRecompensa;
+    uint private totalRecompensa;
+
+    struct detalleUsuario{
+        uint256 deposito;
+        uint256 fechaDeposito;
+    }
+
+    mapping(address => detalleUsuario) public usuarios;
+
+    mapping(address => bool) public usuariosEquipo;
+
     //event 
+    event depositoGanancias(address _from, uint256 _valor, uint256 _fecha);
 
     //modifier
+    modifier soloEquipo() { // Modificador
+        require(usuariosEquipo[msg.sender] == true, "Funcion exclusiva del equipo");
+        _;
+    }
+
+    modifier pasoUnaSemana(){
+        require(block.timestamp >= (ultimaFechaRecompensa + 1 weeks ), "No ha pasado una semana!");
+        _;
+    }
 
     //constructor
 
+    constructor(){
+        totalRecompensa = 0;
+        ultimaFechaRecompensa = 0;
+    }
+
     //funtion
+
+
+    function depositarGanancias() soloEquipo pasoUnaSemana public payable {
+        uint _fechaAnterior =ultimaFechaRecompensa;
+        uint _totalRecompensa = totalRecompensa;
+        totalRecompensa = msg.value;
+        ultimaFechaRecompensa = block.timestamp;
+        emit depositoGanancias(msg.sender , _totalRecompensa, _fechaAnterior);
+
+    }
 
 }
