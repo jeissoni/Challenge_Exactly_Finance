2a0d4700a123393ee21de55a6fdb43b7ecab0031
jeissoni <jeissoni_22@hotmail.com>
2021-12-28 18:02:57 -0500

    the initial version of the tests is added

--- test/testETHPool.ts
+++ test/testETHPool.ts
@@ -1,9 +1,56 @@
 
 import { ethers } from "hardhat";
 import chai, { expect } from 'chai'
+import { BigNumber } from "ethers"
 
 
+describe("test of ETHPools" , function() {
 
 
-const contractFactory = await ethers.getContractFactory("contract");
-const deployed = await contractFactory.deploy();
+    const dataETHPool = async()=>{
+        const [owner, user1] = await ethers.getSigners();
+        const ETHPoolFactory = await ethers.getContractFactory("ETHPool");
+        const ETHPoolDeploy = await ETHPoolFactory.deploy();
+
+        return{
+            owner,
+            user1, 
+            ETHPoolDeploy
+        }
+    }
+
+    describe("test funcionalidad del equipo", function(){
+        it('La cuenta que despliega es la cuenta del equipo', async() => {
+            const {owner, user1, ETHPoolDeploy} = await dataETHPool()
+
+            const esOwnerEquipo : boolean = await ETHPoolDeploy.usuariosEquipo(owner.address)
+            const esUser1Equipo : boolean = await ETHPoolDeploy.usuariosEquipo(user1.address)        
+
+            expect(esOwnerEquipo).to.equal(true)
+            expect(esUser1Equipo).to.equal(false)
+        })
+
+        it('El equipo solo puede depositar despues de una semana', async() =>{
+            const {owner, ETHPoolDeploy} = await dataETHPool()
+
+            await ethers.provider.send("evm_increaseTime", [(60 * 60 * 24 * 7)+1])
+
+            const ganaciasDepositadas: BigNumber = ethers.utils.parseEther("10") 
+
+            await ETHPoolDeploy.depositarGananciasEquipo({value:ganaciasDepositadas})
+
+            const ganaciasContrato : BigNumber = await ETHPoolDeploy.totalRecompensa()
+
+            expect(ganaciasDepositadas).to.equal(ganaciasContrato)
+
+        })
+    })
+
+    
+    describe("test funcionalidad de usuarios", function(){
+    })
+
+
+})
+
+
