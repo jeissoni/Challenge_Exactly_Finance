2e1bb37987645c0c46d26500bef09e8cf1468069
jeissoni <jeissoni_22@hotmail.com>
2021-12-28 18:02:30 -0500

    first version of the function depositarEthUsuario retirarDepositoMasGanancias For tests

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -5,8 +5,8 @@ contract ETHPool {
 
     //state variable
 
-    uint256 private ultimaFechaRecompensa;
-    uint private totalRecompensa;
+    uint256 public ultimaFechaRecompensa;
+    uint256 public totalRecompensa;
 
     struct detalleUsuario{
         uint256 deposito;
@@ -18,7 +18,10 @@ contract ETHPool {
     mapping(address => bool) public usuariosEquipo;
 
     //event 
-    event depositoGanancias(address _from, uint256 _valor, uint256 _fecha);
+    event depositoGananciasEquipo(address _from, uint256 _valor, uint256 _fecha);
+    event depositoEthUsuario(address _from, uint256 _valor, uint256 _fecha);
+    event retirarGanancias(address _from, uint256 _valor, uint256 _fecha);
+
 
     //modifier
     modifier soloEquipo() { // Modificador
@@ -36,18 +39,62 @@ contract ETHPool {
     constructor(){
         totalRecompensa = 0;
         ultimaFechaRecompensa = 0;
+        usuariosEquipo[msg.sender] = true;
     }
 
     //funtion
 
-
-    function depositarGanancias() soloEquipo pasoUnaSemana public payable {
+    function depositarGananciasEquipo() soloEquipo pasoUnaSemana public payable {
         uint _fechaAnterior =ultimaFechaRecompensa;
         uint _totalRecompensa = totalRecompensa;
         totalRecompensa = msg.value;
         ultimaFechaRecompensa = block.timestamp;
-        emit depositoGanancias(msg.sender , _totalRecompensa, _fechaAnterior);
+        emit depositoGananciasEquipo(msg.sender , _totalRecompensa, _fechaAnterior);
+    }
 
+
+    function depositarEthUsuario() public payable {
+       
+        uint _ethAnterior = 0;
+        uint _fechaAnterior = 0;
+
+        //Es la primera vez?
+        if (usuarios[msg.sender].deposito == 0){     
+
+            _ethAnterior = usuarios[msg.sender].deposito;
+            _fechaAnterior = usuarios[msg.sender].fechaDeposito; 
+            
+            usuarios[msg.sender].deposito = _ethAnterior + msg.value;
+            usuarios[msg.sender].fechaDeposito = block.timestamp;
+
+        }else{
+
+            usuarios[msg.sender].deposito = msg.value;
+            usuarios[msg.sender].fechaDeposito = block.timestamp;
+        }
+
+        emit depositoEthUsuario(msg.sender, _ethAnterior, _fechaAnterior);
+    }
+
+
+    function retirarDepositoMasGanancias() public {
+        require(totalRecompensa > 0, "No hay recompensas por entregar");
+        require(usuarios[msg.sender].deposito > 0 , "EL usuarion no a depositado");
+        require(usuarios[msg.sender].fechaDeposito > ultimaFechaRecompensa , "No deposito a tiempo para la recompensa actual");
+
+        uint256 porcentajeGanancia = (usuarios[msg.sender].deposito * 100) / totalRecompensa;
+
+        uint256 ganaciasMasDeposito = usuarios[msg.sender].deposito + (totalRecompensa * porcentajeGanancia);
+
+        usuarios[msg.sender].deposito = 0;
+
+        totalRecompensa = totalRecompensa - ganaciasMasDeposito;
+
+        (bool success,) = payable(msg.sender).call{value: ganaciasMasDeposito} ("");
+
+        require(success, "Transfer failed");
+
+        emit retirarGanancias(msg.sender, ganaciasMasDeposito, block.timestamp);
     }
 
 }
