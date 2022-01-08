fee7e9daaeb84455a4ae4c899479964e5b912b16
jeissoni <jeissoni_22@hotmail.com>
2022-01-08 13:05:40 -0500

    the withdrawal function has been corrected so that the user can recall his funds at any time

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -1,18 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
-
 contract ETHPool {
-
     //state variable
-
     uint256 public ultimaFechaRecompensa;
     uint256 public totalRecompensa;
     uint256 public totalDepositosUsuarios;
 
-    struct detalleUsuario{
+    struct detalleUsuario {
         uint256 deposito;
         uint256 fechaDeposito;
     }
@@ -21,82 +16,105 @@ contract ETHPool {
 
     mapping(address => bool) public usuariosEquipo;
 
-    //event 
-    event depositoGananciasEquipo(address _from, uint256 _valor, uint256 _fecha);
-    event depositoEthUsuario(address _from, uint256 _valor, uint256 _fecha);
-    event retirarGanancias(address _from, uint256 _valor, uint256 _fecha);
-
+    //event
+    event DepositoGananciasEquipo(
+        address _from,
+        uint256 _valor,
+        uint256 _fecha
+    );
+    event DepositoEthUsuario(address _from, uint256 _valor, uint256 _fecha);
+    event RetirarGanancias(address _from, uint256 _valor, uint256 _fecha);
 
     //modifier
-    modifier soloEquipo() { // Modificador
-        require(usuariosEquipo[msg.sender] == true, "Funcion exclusiva del equipo");
+    modifier soloEquipo() {
+        require(
+            usuariosEquipo[msg.sender] == true,
+            "Funcion exclusiva del equipo"
+        );
         _;
     }
 
-    // modifier pasoUnaSemana(){
-    //     require(block.timestamp >= (ultimaFechaRecompensa + 1 weeks ), "No ha pasado una semana!");
-    //     _;
-    // }
-
     //constructor
-
-    constructor(){
-        totalRecompensa = 0;
+    constructor() {
         ultimaFechaRecompensa = block.timestamp;
-        totalDepositosUsuarios = 0;
         usuariosEquipo[msg.sender] = true;
     }
 
     //funtion
+    function depositarGananciasEquipo() public payable soloEquipo {
+        require(
+            block.timestamp > (ultimaFechaRecompensa + 1 weeks),
+            "No ha pasado una semana!"
+        );
 
-    function depositarGananciasEquipo() soloEquipo public payable {
-        require(block.timestamp >= (ultimaFechaRecompensa + 1 weeks ), "No ha pasado una semana!");
-
-        uint _fechaAnterior =ultimaFechaRecompensa;
-        uint _totalRecompensa = totalRecompensa;
+        uint256 _fechaAnterior = ultimaFechaRecompensa;
+        uint256 _totalRecompensa = totalRecompensa;
         totalRecompensa = msg.value;
         ultimaFechaRecompensa = block.timestamp;
-        emit depositoGananciasEquipo(msg.sender , _totalRecompensa, _fechaAnterior);
+        emit DepositoGananciasEquipo(
+            msg.sender,
+            _totalRecompensa,
+            _fechaAnterior
+        );
     }
 
-
     function depositarEthUsuario() public payable {
-       
-        uint _ethAnterior = 0;
-        uint _fechaAnterior = 0;
+        uint256 _ethAnterior = 0;
+        uint256 _fechaAnterior = 0;
 
         _ethAnterior = usuarios[msg.sender].deposito;
-        _fechaAnterior = usuarios[msg.sender].fechaDeposito; 
-       
+        _fechaAnterior = usuarios[msg.sender].fechaDeposito;
+
         usuarios[msg.sender].deposito += msg.value;
-        usuarios[msg.sender].fechaDeposito = block.timestamp;       
+        usuarios[msg.sender].fechaDeposito = block.timestamp;
 
         totalDepositosUsuarios += msg.value;
 
-        emit depositoEthUsuario(msg.sender, _ethAnterior, _fechaAnterior);
+        emit DepositoEthUsuario(msg.sender, _ethAnterior, _fechaAnterior);
     }
-
 
     function retirarDepositoMasGanancias() public {
-        require(totalRecompensa > 0, "No hay recompensas por entregar");
-        require(usuarios[msg.sender].deposito > 0 , "EL usuarion no a depositado");
-        require(usuarios[msg.sender].fechaDeposito < ultimaFechaRecompensa , "No deposito a tiempo para la recompensa actual");
+        //require(totalRecompensa > 0, "No hay recompensas por entregar");
+        //require(usuarios[msg.sender].fechaDeposito < ultimaFechaRecompensa , "No deposito a tiempo para la recompensa actual");
+        require(
+            usuarios[msg.sender].deposito > 0,
+            "EL usuarion no a depositado"
+        );
 
-        uint256 porcentajePool = (usuarios[msg.sender].deposito * 1 ether) / totalDepositosUsuarios;    
+        if (usuarios[msg.sender].fechaDeposito < ultimaFechaRecompensa) {
+            uint256 porcentajePool = (usuarios[msg.sender].deposito * 1 ether) /
+                totalDepositosUsuarios;
 
-        uint256 ganaciasMasDeposito = usuarios[msg.sender].deposito + (totalRecompensa * porcentajePool)/1 ether;     
+            uint256 ganaciasMasDeposito = usuarios[msg.sender].deposito +
+                (totalRecompensa * porcentajePool) /
+                1 ether;
 
-        totalDepositosUsuarios -= usuarios[msg.sender].deposito;
+            totalDepositosUsuarios -= usuarios[msg.sender].deposito;
 
-        totalRecompensa = totalRecompensa - (totalRecompensa * porcentajePool) / 1 ether;
+            totalRecompensa =
+                totalRecompensa -
+                (totalRecompensa * porcentajePool) /
+                1 ether;
 
-        usuarios[msg.sender].deposito = 0;     
+            usuarios[msg.sender].deposito = 0;
 
-        (bool success,) = payable(msg.sender).call{value: ganaciasMasDeposito} ("");
+            (bool success, ) = payable(msg.sender).call{
+                value: ganaciasMasDeposito
+            }("");
 
-        require(success, "Transfer failed");
+            require(success, "Transfer failed");
 
-        emit retirarGanancias(msg.sender, ganaciasMasDeposito, block.timestamp);
+            emit RetirarGanancias(
+                msg.sender,
+                ganaciasMasDeposito,
+                block.timestamp
+            );
+        } else {
+            (bool success, ) = payable(msg.sender).call{
+                value: usuarios[msg.sender].deposito
+            }("");
+
+            require(success, "Transfer failed");
+        }
     }
-
 }
