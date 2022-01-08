b2a864b722461c242ff8650ff8fd6baf9b862f00
jeissoni <jeissoni_22@hotmail.com>
2022-01-08 13:02:45 -0500

    variables are added for display
    parameters are added for deployment and validation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,14 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+
+require("dotenv").config();
+
+
+
+const projeId=process.env.INFURA_PROJECT_ID
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY
+const etherscanApi = process.env.ETHERSCAN_API_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,4 +28,15 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks:{
+    rinkeby:{
+      url: `https://rinkeby.infura.io/v3/${projeId}`,
+      accounts:[       
+      privateKey
+      ]
+    }
+  },
+  etherscan: {
+    apiKey: etherscanApi
+  }
 };
