5bba6a5f71607f6851ac6d1876c031d3ede1f9ea
jeissoni_22 <jeissoni_22@hotmail.com>
2022-01-10 17:31:18 -0500

    script is added to query the ETH blocked in the contract

--- scripts/interactWithContract.ts
+++ scripts/interactWithContract.ts
@@ -1,17 +1,47 @@
-import { ethers } from "ethers"
+import { BigNumber } from "ethers"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { ethers } from "hardhat"
+
+import {abi} from '../artifacts/contracts/ETHPool.sol/ETHPool.json'
 
 
+
+var addressContract : string = ""
 
 async function balance() {
 
-    const address = "0xc3fB9F429Faa1810F390c3f55E385479ca5059d0"
+    
+    var deposito: BigNumber = ethers.utils.parseEther("10")
 
-    const provider = ethers.getDefaultProvider()
+    const [owner, user1, user2, user3, user4] =  await ethers.getSigners()
+    var userArray: SignerWithAddress[] = [owner, user1, user2, user3, user4]
 
-    var balanceContract = ethers.utils.formatEther( await provider.getBalance(address))
+    const ETHPoolFactory = await ethers.getContractFactory("ETHPool");
+    const ETHPoolDeploy = await ETHPoolFactory.deploy();
 
-    return { balanceContract }
+    addressContract = ETHPoolDeploy.address
+    
+    for (const item of userArray){
+        await ETHPoolDeploy.connect(item).depositarEthUsuario({ value: deposito })
+    }
+    // leer los eventos especificos de un contrato
+    // let eventFilter = ETHPoolDeploy.filters.DepositoEthUsuario()
+    // let events = await ETHPoolDeploy.queryFilter(eventFilter)
+
+    var balanceETHEquipo = await ETHPoolDeploy.totalRecompensa()
+    var balanceUsuarios = await ETHPoolDeploy.totalDepositosUsuarios()
+    var totalETHBloqueado  = balanceETHEquipo.add(balanceETHEquipo)
+
+    var mensaje  = "Total ETH bloquedao : " 
+                    +ethers.utils.formatEther(totalETHBloqueado) + " | " 
+                    +"Total ETH Equipo bloquedo : " 
+                    +ethers.utils.formatEther(balanceETHEquipo) + " | "
+                    +"Total ETH Usuarios bloqueado : "
+                    +ethers.utils.formatEther(balanceUsuarios)
+
+
+    return { mensaje }
 } 
 
- balance().then((x)=> console.log(x.balanceContract))
+ balance().then((x)=> console.log(x.mensaje))
 
