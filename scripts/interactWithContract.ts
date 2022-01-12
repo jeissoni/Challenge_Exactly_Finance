20d6f1cbed30656eddf2494044fc3539be4b93e1
jeissoni_22 <jeissoni_22@hotmail.com>
2022-01-11 19:54:14 -0500

    code is added to be able to interact with the contract in the test network

--- scripts/interactWithContract.ts
+++ scripts/interactWithContract.ts
@@ -1,17 +1,12 @@
-import { BigNumber } from "ethers"
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
-import { ethers } from "hardhat"
+
+import { ethers } from "ethers";
 
 import {abi} from '../artifacts/contracts/ETHPool.sol/ETHPool.json'
 require("dotenv").config();
 
-
-
-var addressContract : string = ""
-
 async function balance() {
 
-    const address = "0x6F7092Ad8084C708A5D6Fa9CeFaAff86921e7fBf"
+    const address = "0xfD24E1f372B82B473E4a42248CAF810304606E71"
 
     const projeId=process.env.INFURA_PROJECT_ID
 
@@ -20,36 +15,14 @@ async function balance() {
 
     var contractETHPool  = new ethers.Contract( address , abi , provider )
 
-    var balanceETHEquipo = await contractETHPool.totalRecompensa()
-    var balanceUsuarios = await contractETHPool.totalDepositosUsuarios()
-
-
-    // var deposito: BigNumber = ethers.utils.parseEther("10")
-
-    // const [owner, user1, user2, user3, user4] =  await ethers.getSigners()
-    // var userArray: SignerWithAddress[] = [owner, user1, user2, user3, user4]
-
-    // const ETHPoolFactory = await ethers.getContractFactory("ETHPool");
-    // const ETHPoolDeploy = await ETHPoolFactory.deploy();
-
-    // addressContract = ETHPoolDeploy.address
+    var balanceETHEquipo = await contractETHPool.totalReward()
+    var balanceUsuarios = await contractETHPool.totalUserDeposits()
     
-    // for (const item of userArray){
-    //     await ETHPoolDeploy.connect(item).depositarEthUsuario({ value: deposito })
-    // }
-    // // leer los eventos especificos de un contrato
-    // // let eventFilter = ETHPoolDeploy.filters.DepositoEthUsuario()
-    // // let events = await ETHPoolDeploy.queryFilter(eventFilter)
 
-    // var balanceETHEquipo = await ETHPoolDeploy.totalRecompensa()
-    // var balanceUsuarios = await ETHPoolDeploy.totalDepositosUsuarios()
-    // var totalETHBloqueado  = balanceETHEquipo.add(balanceETHEquipo)
-
-    var mensaje  = "Total ETH Equipo bloquedo : " 
-                    +ethers.utils.formatEther(balanceETHEquipo) + " | "
-                    +"Total ETH Usuarios bloqueado : "
-                    +ethers.utils.formatEther(balanceUsuarios)
-
+    var mensaje  = "Total ETH Team locked : " 
+                     +ethers.utils.formatEther(balanceETHEquipo) + " | "
+                     +"Total ETH User locked : "
+                     +ethers.utils.formatEther(balanceUsuarios)
 
     return { mensaje }
 } 
